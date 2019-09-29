b33986cda5b5a90f060c23361b44716a6c3d3fa2
Niclas <svardl@kth.se>
2019-09-29 19:22:24 +0200

    commit

---
+++ signinpar2/signinpar2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace signinpar2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            this.Userbox = new System.Windows.Forms.TextBox();
+            this.Passbox = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.Location = new System.Drawing.Point(79, 36);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(113, 26);
+            label1.TabIndex = 1;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label2.Location = new System.Drawing.Point(79, 148);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(113, 26);
+            label2.TabIndex = 3;
+            label2.Text = "Username";
+            label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Userbox
+            // 
+            this.Userbox.Location = new System.Drawing.Point(84, 76);
+            this.Userbox.Name = "Userbox";
+            this.Userbox.Size = new System.Drawing.Size(282, 26);
+            this.Userbox.TabIndex = 0;
+            // 
+            // Passbox
+            // 
+            this.Passbox.Location = new System.Drawing.Point(84, 195);
+            this.Passbox.Name = "Passbox";
+            this.Passbox.Size = new System.Drawing.Size(282, 26);
+            this.Passbox.TabIndex = 2;
+            this.Passbox.UseSystemPasswordChar = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(80, 336);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(118, 20);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Create account";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.Location = new System.Drawing.Point(84, 256);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(108, 37);
+            this.loginBtn.TabIndex = 5;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(400, 196);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(147, 24);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Show password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 385);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.Passbox);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.Userbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Userbox;
+        private System.Windows.Forms.TextBox Passbox;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ signinpar2/signinpar2/Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace signinpar2
+{
+    public partial class Form2 : Form
+    {
+        List<string> user;
+        string path = Directory.GetCurrentDirectory() + "\\Userpass.csv";
+        public Form2()
+        {
+            //MessageBox.Show(path);
+            var reader = new StreamReader(path);
+
+            user = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                user.Add(values[0]);
+            }
+            reader.Close();
+            InitializeComponent();
+            takenLab.Visible = false;
+        }
+
+        private void creareBtn_Click(object sender, EventArgs e){
+         
+
+            if (user.Contains(newuserbox.Text)) {
+                MessageBox.Show("That name is taken");
+                return;
+            
+            }
+
+            string account = newuserbox.Text + "," + newpasswordbox.Text;
+            File.AppendAllText(path, "\n"+account);
+            MessageBox.Show("Account created!");
+            this.Close();
+            this.Dispose();
+
+        }
+
+        private void showBtn_CheckedChanged(object sender, EventArgs e){
+            newpasswordbox.UseSystemPasswordChar = !showBtn.Checked;
+        }
+
+        private void newuserbox_TextChanged(object sender, EventArgs e)
+        {
+            if (user.Contains(newuserbox.Text))
+            {
+                takenLab.Visible = true;
+            }
+            else {
+                takenLab.Visible = false;
+            }
+        }
+    }
+}
